b0b97d9051f69c04199c2742adb264df96f38bb6
Amit Jain <ajain@boisestate.edu>
2019-10-13 21:44:56 -0600

    Update first spark example

--- examples/Spark/simpleapp/src/main/java/SimpleApp.java
+++ examples/Spark/simpleapp/src/main/java/SimpleApp.java
@@ -6,17 +6,28 @@ import org.apache.spark.sql.Dataset;
 
 public class SimpleApp {
   public static void main(String[] args) {
-    String logFile = "/home/amit/spark-install/spark/README.md"; // Should be some file on your system
     SparkConf conf = new SparkConf().setAppName("SimpleApp").setMaster("local");
     JavaSparkContext sc = new JavaSparkContext(conf);
-    //SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
-    //Dataset<String> logData = spark.read().textFile(logFile).cache();
-    JavaRDD<String> logData = sc.textFile(logFile);
 
+    String logFile = "/home/amit/spark-install/spark/README.md"; //Update to your file path 
+    JavaRDD<String> logData = sc.textFile(logFile);
 
     long numAs = logData.filter(s -> s.contains("a")).count();
     long numBs = logData.filter(s -> s.contains("b")).count();
-
+    
+    System.out.println();
     System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+    System.out.println();
+
+    
+    JavaRDD<Integer> lineLengths = logData.map(s -> s.length());
+    int totalLength = lineLengths.reduce((a, b) -> a + b);
+    
+    System.out.println();
+    System.out.println("Total linelengths = " + totalLength);
+    System.out.println();
+
+
+    sc.stop(); // stop the Spark context
   }
 }
