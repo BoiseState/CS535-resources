f1bcd7b877a957090f3e4a0e18713c1c286cb729
Amit Jain <ajain@boisestate.edu>
2019-10-23 13:39:36 -0600

    Add new example to show logging and meta-information

--- examples/Spark/simpleapp/README.md
+++ examples/Spark/simpleapp/README.md
@@ -5,6 +5,9 @@ Build
 mvn clean
 mvn package
 
+or build the jar file directly in Eclipse (just include all Spark jar files from
+~/spark-install/spark/jars as External jars for the project.
+
 Run
 ===
 
@@ -12,3 +15,23 @@ spark-submit --class "SimpleApp" --master local[4] target/simple-project-1.0.jar
 
 where local[4] says to use 4 threads on local machine. You can change that to higher or lower
 or replace by * to use as many threads as the number of logical threads on your local system.
+
+There are a lot of info messages. You can redirect them to a file as follows:
+
+spark-submit --class "SimpleApp" --master local[4] target/simple-project-1.0.jar 2> log
+
+Or you can control the logging level from your program. See SimpleApp2.java for an example. Here is
+the relevant code:
+
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+. . .
+
+Logger log = LogManager.getRootLogger();
+log.setLevel(Level.WARN);
+
+
+You can also configure Spark defaults by creating a spark/conf/log4j.properties file. This is
+a bit more complex but would make more sense in a production environment.
--- examples/Spark/simpleapp/src/main/java/SimpleApp2.java
+++ examples/Spark/simpleapp/src/main/java/SimpleApp2.java
@@ -1,16 +1,25 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.storage.StorageLevel;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
-
-public class SimpleApp {
+public class SimpleApp2 {
   public static void main(String[] args) {
+      
+    Logger log = LogManager.getRootLogger();
+    log.setLevel(Level.WARN);
+    
     SparkConf conf = new SparkConf().setAppName("SimpleApp");
     JavaSparkContext sc = new JavaSparkContext(conf);
 
     String logFile = "/home/amit/spark-install/spark/README.md"; //Update to your file path 
     JavaRDD<String> logData = sc.textFile(logFile);
+    
+    System.out.println("#partitions " + logData.getNumPartitions());
+    System.out.println("#storage level " + logData.getStorageLevel());
+
 
     long numAs = logData.filter(s -> s.contains("a")).count();
     long numBs = logData.filter(s -> s.contains("b")).count();
@@ -21,6 +30,9 @@ public class SimpleApp {
 
     
     JavaRDD<Integer> lineLengths = logData.map(s -> s.length());
+    System.out.println("#partitions " + lineLengths.getNumPartitions());
+    System.out.println("#storage level " + lineLengths.getStorageLevel());
+
     int totalLength = lineLengths.reduce((a, b) -> a + b);
     
     System.out.println();
