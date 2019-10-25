94da07152360e6bdc6b17b1dbfb6771b3f87b947
Amit Jain <ajain@boisestate.edu>
2019-10-25 15:41:28 -0600

    Update wordcount

--- examples/Spark/wordcount/WordCount.java
+++ examples/Spark/wordcount/WordCount.java
@@ -17,42 +17,40 @@
 
 /* modified by amit */
 
-
 import scala.Tuple2;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.SparkSession;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public final class WordCount {
-  private static final Pattern SPACE = Pattern.compile(" ");
+public final class WordCount
+{
+    private static final Pattern SPACE = Pattern.compile(" ");
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    if (args.length < 1) {
-      System.err.println("Usage: JavaWordCount <file>");
-      System.exit(1);
+	if (args.length < 1) {
+	    System.err.println("Usage: JavaWordCount <file or folder>");
+	    System.exit(1);
+	}
+
+	SparkConf conf = new SparkConf().setAppName("WordCount");
+	JavaSparkContext sc = new JavaSparkContext(conf);
+
+	JavaRDD<String> lines = sc.textFile(args[0]);
+	JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+	JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+	JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+	List<Tuple2<String, Integer>> output = counts.collect();
+	for (Tuple2<?, ?> tuple : output) {
+	    System.out.println(tuple._1() + ": " + tuple._2());
+	}
+	sc.stop();
+	sc.close();
     }
-
-	SparkConf conf = new SparkConf (). setAppName(" WordCount ");
- 	JavaSparkContext sc = new JavaSparkContext(conf);
-
-    JavaRDD<String> lines = sc.textFile(args[0]);
-    JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-    JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-
-    List<Tuple2<String, Integer>> output = counts.collect();
-    for (Tuple2<?,?> tuple : output) {
-      System.out.println(tuple._1() + ": " + tuple._2());
-    }
-    sc.stop();
-    sc.close();
-  }
 }
