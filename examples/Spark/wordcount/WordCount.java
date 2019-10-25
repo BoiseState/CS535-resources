573cdf966efa375f58a642e4e7b0deb9a34c52ce
Amit Jain <ajain@boisestate.edu>
2019-10-25 12:53:26 -0600

    add wordcount example

--- examples/Spark/wordcount/WordCount.java
+++ examples/Spark/wordcount/WordCount.java
@@ -1,43 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.lang.Iterable;
+/* modified by amit */
+
 
 import scala.Tuple2;
 
-import org.apache.commons.lang.StringUtils;
-
-import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.SparkSession;
 
-public class WordCount
-{
-    @SuppressWarnings("serial")
-    public static void main(String[] args) throws Exception {
-	String master = args[0];
-	JavaSparkContext sc = new JavaSparkContext(master, "wordcount", System.getenv("SPARK_HOME"),
-	        System.getenv("JARS"));
-	JavaRDD<String> rdd = sc.textFile(args[1]);
-	JavaPairRDD<String, Integer> counts = rdd.flatMap(new FlatMapFunction<String, String>() {
-	    public List<String> call(String x) {
-		return Arrays.asList(x.split(" "));
-	    }
-	}).mapToPair(new PairFunction<String, String, Integer>() {
-	    public Tuple2<String, Integer> call(String x) {
-		return new Tuple2<String, Integer>(x, 1);
-	    }
-	}).reduceByKey(new Function2<Integer, Integer, Integer>() {
-	    public Integer call(Integer x, Integer y) {
-		return x + y;
-	    }
-	});
-	counts.saveAsTextFile(args[2]);
-	sc.close();
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public final class WordCount {
+  private static final Pattern SPACE = Pattern.compile(" ");
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length < 1) {
+      System.err.println("Usage: JavaWordCount <file>");
+      System.exit(1);
     }
 
+	SparkConf conf = new SparkConf (). setAppName(" WordCount ");
+ 	JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaRDD<String> lines = sc.textFile(args[0]);
+    JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+    JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+    List<Tuple2<String, Integer>> output = counts.collect();
+    for (Tuple2<?,?> tuple : output) {
+      System.out.println(tuple._1() + ": " + tuple._2());
+    }
+    sc.stop();
+    sc.close();
+  }
 }
