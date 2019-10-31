326c87d0c2dea2f907b85367889944619f115fb1
Amit Jain <ajain@boisestate.edu>
2019-10-31 12:58:08 -0600

    Update case analysis spark example

--- examples/Spark/case-analysis/CaseAnalysis.java
+++ examples/Spark/case-analysis/CaseAnalysis.java
@@ -9,13 +9,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 public final class CaseAnalysis {
-	private static final Pattern SPACE = Pattern.compile(" ");
-
 	public static void main(String[] args) throws Exception {
 
 		if (args.length < 1) {
@@ -27,15 +26,32 @@ public final class CaseAnalysis {
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
 		JavaRDD<String> lines = sc.textFile(args[0]);
-		JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-		JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-		JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-		counts.saveAsTextFile("hdfs://localhost:9000/user/amit/output");
+		JavaRDD<String> letters = lines.flatMap(s -> Arrays.asList(s.split("")).iterator());
+		System.out.println("count = " + letters.count());
 
-		// List<Tuple2<String, Integer>> output = counts.collect();
-		// for (Tuple2<?, ?> tuple : output) {
-		// System.out.println(tuple._1() + ": " + tuple._2());
-		// }
+		JavaPairRDD<String, Integer> ones = letters.mapToPair(s -> {
+			Tuple2<String, Integer> t = new Tuple2<String, Integer>("other", 1);
+			if (s.length() > 0) {
+				if (Character.isUpperCase(s.charAt(0)))
+					t = new Tuple2<>(s.toUpperCase(), 1);
+				else if (Character.isLowerCase(s.charAt(0)))
+					t = new Tuple2<>(s.toUpperCase(), 0);
+			}
+			return t;
+		});
+		
+		JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+		//counts.saveAsTextFile("hdfs://localhost:9000/user/amit/output");
+		counts.saveAsTextFile("output");
+		List<Tuple2<String, Integer>> output = counts.collect();
+		for (Tuple2<?, ?> tuple : output) {
+			System.out.println(tuple._1() + ": " + tuple._2());
+		}
+		
+		Map<String, Long> totals = ones.countByKey();
+		totals.forEach((key, value) -> System.out.println(key + " " + value));
+		System.out.println();
+
 		sc.stop();
 		sc.close();
 	}
