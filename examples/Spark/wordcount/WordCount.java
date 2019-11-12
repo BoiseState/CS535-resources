54255d27c5ece0182dc0d33e7534d7cf9af931fa
Amit Jain <ajain@boisestate.edu>
2019-11-12 16:23:43 -0700

    Add jar file

--- examples/Spark/wordcount/WordCount.java
+++ examples/Spark/wordcount/WordCount.java
@@ -45,12 +45,12 @@ public final class WordCount
 	JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
 	JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
 	JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-	counts.saveAsTextFile("hdfs://localhost:9000/user/amit/output");
+	//counts.saveAsTextFile("hdfs://localhost:9000/user/amit/output");
 
-//	List<Tuple2<String, Integer>> output = counts.collect();
-//	for (Tuple2<?, ?> tuple : output) {
-//	    System.out.println(tuple._1() + ": " + tuple._2());
-//	}
+	List<Tuple2<String, Integer>> output = counts.collect();
+	for (Tuple2<?, ?> tuple : output) {
+	    System.out.println(tuple._1() + ": " + tuple._2());
+	}
 	sc.stop();
 	sc.close();
     }
