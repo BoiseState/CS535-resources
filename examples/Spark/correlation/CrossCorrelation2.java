424ea41af1d6c9d82b13b0dabfd5597c8ea43a80
Amit Jain <ajain@boisestate.edu>
2019-11-20 15:53:53 -0700

    Update examples

--- examples/Spark/correlation/CrossCorrelation2.java
+++ examples/Spark/correlation/CrossCorrelation2.java
@@ -17,7 +17,6 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
