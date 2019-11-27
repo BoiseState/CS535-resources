dbebc32bee9a3257012d1ac52843a13293e0f2a7
Amit Jain <ajain@boisestate.edu>
2019-11-27 13:14:37 -0700

    Add findnodes.sh script

---
+++ examples/Hadoop/local-scripts/findnodes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+case $# in 
+0) echo "Usage $0 <#nodes>"; exit 1;;
+esac
+
+
+request=$1
+num=0
+#generate random startnode at 10 or higher
+startnode=$[10 + 8 *(RANDOM % 10)]
+echo -n > workers #create new empty file
+for i in {1..100}
+do 
+	node=onyxnode$[startnode+i]
+	timeout 1 ssh -o stricthostkeychecking=no $node  date >& /dev/null
+	if test "$?" = "0" 
+	then
+		echo "$node" >> workers
+		num=$[num+1]
+		echo "Found $num good nodes so far"
+		if test "$num" = "$request"
+		then
+			echo "Done. Check node names in file named workers"
+			exit
+		fi
+	fi 
+done
+
