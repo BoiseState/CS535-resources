de82123826be3c0b53c84124adefed50c0efef9a
Amit Jain <ajain@boisestate.edu>
2019-12-03 13:19:06 -0700

    Update Spark SQL example

--- examples/Spark/spark-sql-example/JavaSparkSQLExample3.java
+++ examples/Spark/spark-sql-example/JavaSparkSQLExample3.java
@@ -21,10 +21,6 @@
 // $example on:programmatic_schema$
 import java.util.ArrayList;
 import java.util.List;
-// $example off:programmatic_schema$
-// $example on:create_ds$
-import java.util.Arrays;
-import java.util.Collections;
 import java.io.Serializable;
 // $example off:create_ds$
 
@@ -96,163 +92,17 @@ public class JavaSparkSQLExample3 {
       .getOrCreate();
     // $example off:init_session$
 
-    runBasicDataFrameExample(spark);
-    runDatasetCreationExample(spark);
     runInferSchemaExample(spark);
     runProgrammaticSchemaExample(spark);
 
     spark.stop();
   }
 
-  private static void runBasicDataFrameExample(SparkSession spark) throws AnalysisException {
-    // $example on:create_df$
-    Dataset<Row> df = spark.read().json("people.json");
-
-    // Displays the content of the DataFrame to stdout
-    df.show();
-    // +----+-------+
-    // | age|   name|
-    // +----+-------+
-    // |null|Michael|
-    // |  30|   Andy|
-    // |  19| Justin|
-    // +----+-------+
-    // $example off:create_df$
-
-    // $example on:untyped_ops$
-    // Print the schema in a tree format
-    df.printSchema();
-    // root
-    // |-- age: long (nullable = true)
-    // |-- name: string (nullable = true)
-
-    // Select only the "name" column
-    df.select("name").show();
-    // +-------+
-    // |   name|
-    // +-------+
-    // |Michael|
-    // |   Andy|
-    // | Justin|
-    // +-------+
-
-    // Select everybody, but increment the age by 1
-    df.select(col("name"), col("age").plus(1)).show();
-    // +-------+---------+
-    // |   name|(age + 1)|
-    // +-------+---------+
-    // |Michael|     null|
-    // |   Andy|       31|
-    // | Justin|       20|
-    // +-------+---------+
-
-    // Select people older than 21
-    df.filter(col("age").gt(21)).show();
-    // +---+----+
-    // |age|name|
-    // +---+----+
-    // | 30|Andy|
-    // +---+----+
-
-    // Count people by age
-    df.groupBy("age").count().show();
-    // +----+-----+
-    // | age|count|
-    // +----+-----+
-    // |  19|    1|
-    // |null|    1|
-    // |  30|    1|
-    // +----+-----+
-    // $example off:untyped_ops$
-
-    // $example on:run_sql$
-    // Register the DataFrame as a SQL temporary view
-    df.createOrReplaceTempView("people");
-
-    Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
-    sqlDF.show();
-    // +----+-------+
-    // | age|   name|
-    // +----+-------+
-    // |null|Michael|
-    // |  30|   Andy|
-    // |  19| Justin|
-    // +----+-------+
-    // $example off:run_sql$
-
-    // $example on:global_temp_view$
-    // Register the DataFrame as a global temporary view
-    df.createGlobalTempView("people");
-
-    // Global temporary view is tied to a system preserved database `global_temp`
-    spark.sql("SELECT * FROM global_temp.people").show();
-    // +----+-------+
-    // | age|   name|
-    // +----+-------+
-    // |null|Michael|
-    // |  30|   Andy|
-    // |  19| Justin|
-    // +----+-------+
-
-    // Global temporary view is cross-session
-    spark.newSession().sql("SELECT * FROM global_temp.people").show();
-    // +----+-------+
-    // | age|   name|
-    // +----+-------+
-    // |null|Michael|
-    // |  30|   Andy|
-    // |  19| Justin|
-    // +----+-------+
-    // $example off:global_temp_view$
-  }
-
-  private static void runDatasetCreationExample(SparkSession spark) {
-    // $example on:create_ds$
-    // Create an instance of a Bean class
-    Person person = new Person();
-    person.setName("Andy");
-    person.setAge(32);
-
-    // Encoders are created for Java beans
-    Encoder<Person> personEncoder = Encoders.bean(Person.class);
-    Dataset<Person> javaBeanDS = spark.createDataset(
-      Collections.singletonList(person),
-      personEncoder
-    );
-    javaBeanDS.show();
-    // +---+----+
-    // |age|name|
-    // +---+----+
-    // | 32|Andy|
-    // +---+----+
-
-    // Encoders for most common types are provided in class Encoders
-    Encoder<Integer> integerEncoder = Encoders.INT();
-    Dataset<Integer> primitiveDS = spark.createDataset(Arrays.asList(1, 2, 3), integerEncoder);
-    Dataset<Integer> transformedDS = primitiveDS.map(
-        (MapFunction<Integer, Integer>) value -> value + 1,
-        integerEncoder);
-    transformedDS.collect(); // Returns [2, 3, 4]
-
-    // DataFrames can be converted to a Dataset by providing a class. Mapping based on name
-    String path = "examples/src/main/resources/people.json";
-    Dataset<Person> peopleDS = spark.read().json(path).as(personEncoder);
-    peopleDS.show();
-    // +----+-------+
-    // | age|   name|
-    // +----+-------+
-    // |null|Michael|
-    // |  30|   Andy|
-    // |  19| Justin|
-    // +----+-------+
-    // $example off:create_ds$
-  }
-
   private static void runInferSchemaExample(SparkSession spark) {
     // $example on:schema_inferring$
     // Create an RDD of Person objects from a text file
     JavaRDD<Person> peopleRDD = spark.read()
-      .textFile("examples/src/main/resources/people.txt")
+      .textFile("people.txt")
       .javaRDD()
       .map(line -> {
         String[] parts = line.split(",");
@@ -299,7 +149,7 @@ public class JavaSparkSQLExample3 {
     // $example on:programmatic_schema$
     // Create an RDD
     JavaRDD<String> peopleRDD = spark.sparkContext()
-      .textFile("examples/src/main/resources/people.txt", 1)
+      .textFile("people.txt", 1)
       .toJavaRDD();
 
     // The schema is encoded in a string
