8a10340428bc00571e8ea7ff63f1124fcb8a33c6
Amit Jain <ajain@boisestate.edu>
2019-12-10 13:29:20 -0700

    Update streaming example

--- examples/Spark/streaming/JavaStructuredNetworkWordCount.java
+++ examples/Spark/streaming/JavaStructuredNetworkWordCount.java
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/* modified by amit */
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
@@ -31,21 +13,21 @@ import org.apache.spark.sql.streaming.StreamingQuery;
  * Counts words in UTF8 encoded, '\n' delimited text received from the network
  * every second.
  *
- * Usage: JavaNetworkWordCount <hostname> <port> <hostname> and <port> describe
+ * Usage: JavaStructuredNetworkWordCount <hostname> <port> <hostname> and <port> describe
  * the TCP server that Spark Streaming would connect to receive data.
  *
  * To run this on your local machine, you need to first run a Netcat server `$
  * nc -lk 9999` and then run the example `$ bin/run-example
  * org.apache.spark.examples.streaming.JavaNetworkWordCount localhost 9999`
  */
-public final class JavaNetworkWordCount
+public final class JavaStructuredNetworkWordCount
 {
     private static final Pattern SPACE = Pattern.compile(" ");
 
 
     public static void main(String[] args) throws Exception {
 	if (args.length < 2) {
-	    System.err.println("Usage: JavaNetworkWordCount <hostname> <port>");
+	    System.err.println("Usage: JavaStructuredNetworkWordCount <hostname> <port>");
 	    System.exit(1);
 	}
 
