5858fc9bd4684a04582131829774bbc592a547dd
Amit Jain <ajain@boisestate.edu>
2021-09-16 13:15:28 -0600

    Add streaming Java example

---
+++ examples/Hadoop/myExamples/word-count/streaming-java/Mapper.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Mapper {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		while (stdin.hasNextLine()){
+			String line = stdin.nextLine();
+			String[] words = line.split("[ \t\n\r]");
+			for (String word: words) {
+				System.out.println(word + "\t" + "1");
+			}
+		}
+		stdin.close();
+	}
+}
---
+++ examples/Hadoop/myExamples/word-count/streaming-java/Reducer.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Reducer {
+
+	public static void main(String[] args) {
+		long count = 0;
+		String currentWord = "";
+		
+		Scanner stdin = new Scanner(System.in);
+		while (stdin.hasNextLine()){
+			String line = stdin.nextLine().trim();
+			String[] field = line.split("\t",0);
+			if (field[0].compareTo(currentWord) == 0) 
+				count += Integer.parseInt(field[1]);
+			else {
+				if (count > 0) System.out.println(currentWord + "\t" + count);
+				count = Integer.parseInt(field[1]);
+				currentWord = field[0];
+			}	
+		}
+		stdin.close();
+		System.out.println(currentWord + "\t" + count);
+	}
+}
