d3fbcdba1d199714b4a02704c66b390040a31449
Amit Jain <ajain@boisestate.edu>
2021-09-28 10:40:27 -0600

    Update examples

--- examples/Hadoop/myExamples/tf-idf/src/TfIdfDriver.java
+++ examples/Hadoop/myExamples/tf-idf/src/TfIdfDriver.java
@@ -31,7 +31,7 @@ public class TfIdfDriver
 				.getRemainingArgs();
 
 		if (otherArgs.length != 2) {
-			System.err.println("Usage: inverted-index <input> <output>");
+			System.err.println("Usage: TfIdfDriver <input> <output>");
 			System.exit(2);
 		}
 
