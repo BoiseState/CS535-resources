b1fe90968e2ccbec360090180e042ddcdf553e80
amit <ajain@boisestate.edu>
2021-10-06 13:21:08 -0600

    Update local scripts

--- examples/Hadoop/local-scripts/hdfs-mkusers.sh
+++ examples/Hadoop/local-scripts/hdfs-mkusers.sh
@@ -7,5 +7,18 @@ do
 	echo $userName
 	hdfs dfs -mkdir /user/$userName
 	hdfs dfs -chmod 700 /user/$userName
+	hdfs dfs -chown $userName /user/$userName
 	hdfs dfs -ls -d /user/$userName
 done
+
+echo "setting up staging folders in DFS"
+
+hdfs dfs -mkdir /tmp
+hdfs dfs -chmod 1777 /tmp
+hdfs dfs -mkdir /tmp/hadoop-yarn
+hdfs dfs -chmod 777 /tmp/hadoop-yarn
+hdfs dfs -mkdir /tmp/hadoop-yarn/staging
+hdfs dfs -chmod 777 /tmp/hadoop-yarn/staging
+hdfs dfs -mkdir /tmp/hadoop-yarn/staging/history
+hdfs dfs -chmod 777 /tmp/hadoop-yarn/staging/history
+
