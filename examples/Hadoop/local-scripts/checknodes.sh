7022e72aec31cfac8b2f6c4abad36dd9c3011542
Amit Jain <ajain@boisestate.edu>
2021-10-19 17:20:06 -0600

    Update findnodes and checknodes scripts

--- examples/Hadoop/local-scripts/checknodes.sh
+++ examples/Hadoop/local-scripts/checknodes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo
 echo "=== checknodes.sh: workers file from the folder $(pwd) ==="
 echo
 
@@ -9,3 +8,6 @@ do
 	echo "=== Checking node $host for already running Hadoop ==="
 	timeout 1 ssh -o stricthostkeychecking=no $host "ps augx | grep java | grep hadoop | grep -v grep"
 done
+	echo
+	echo "Remove nodes from workers file that had hadoop already running on it!"
+	echo
