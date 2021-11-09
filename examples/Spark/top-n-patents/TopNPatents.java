d17e09712297f7e6b431f5978a3be53edf6d7d98
Amit Jain <ajain@boisestate.edu>
2021-11-09 13:06:26 -0700

    Update top n patents spark example

--- examples/Spark/top-n-patents/TopNPatents.java
+++ examples/Spark/top-n-patents/TopNPatents.java
@@ -19,57 +19,53 @@ import org.apache.spark.api.java.JavaSparkContext;
 import java.util.List;
 import java.util.regex.Pattern;
 
-public final class TopNPatents {
-	private static final Pattern SPACE = Pattern.compile("\\s+"); // one or more spaces
+public final class TopNPatents
+{
+    private static final Pattern SPACE = Pattern.compile("\\s+"); // one or more spaces
 
-	public static void printRDD(JavaPairRDD<Integer, Integer> rdd) {
-		List<Tuple2<Integer, Integer>> output = rdd.collect();
-		System.out.println();
-		for (Tuple2<?, ?> tuple : output) {
-			System.out.println("(" + tuple._1() + "," + tuple._2() + ")");
-		}
-		System.out.println();
+    public static void printRDD(JavaPairRDD<Integer, Integer> rdd) {
+	System.out.println();
+	rdd.collect().forEach(x -> System.out.println(x));
+	System.out.println();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+	if (args.length < 1) {
+	    System.err.println("Usage: java TopNPatents <file or folder> <N>");
+	    System.exit(1);
 	}
 
-	public static void main(String[] args) throws Exception {
+	SparkConf conf = new SparkConf().setAppName("TopNPatents");
+	JavaSparkContext sc = new JavaSparkContext(conf);
 
-		if (args.length < 1) {
-			System.err.println("Usage: java TopNPatents <file or folder> <N>");
-			System.exit(1);
-		}
+	JavaRDD<String> lines = sc.textFile(args[0]);
+	int num = Integer.parseInt(args[1]);
 
-		SparkConf conf = new SparkConf().setAppName("TopNPatents");
-		JavaSparkContext sc = new JavaSparkContext(conf);
+	// create one tuple for each citation of the form (patent#, 1)
+	JavaPairRDD<Integer, Integer> patents = lines
+	        .mapToPair(s -> new Tuple2<>(Integer.parseInt(SPACE.split(s)[1]), 1));
+	printRDD(patents);
 
-		JavaRDD<String> lines = sc.textFile(args[0]);
-		int num = Integer.parseInt(args[1]);
+	// count the number of references to each patent that was cited
+	patents = patents.reduceByKey((i, j) -> i + j);
+	printRDD(patents);
 
-		// create one tuple for each citation of the form (patent#, 1)
-		JavaPairRDD<Integer, Integer> patents = 
-			lines.mapToPair(s -> new Tuple2<>(Integer.parseInt(SPACE.split(s)[1]), 1));
-		printRDD(patents);
+	// Swap each tuple so we can sort by number of citations
+	JavaPairRDD<Integer, Integer> swappedPatents = patents.mapToPair(s -> s.swap());
+	printRDD(swappedPatents);
 
-		// count the number of references to each patent that was cited
-		patents = patents.reduceByKey((i, j) -> i + j);
-		printRDD(patents);
+	// Sort in descending order by number of citations
+	swappedPatents = swappedPatents.sortByKey(false); // false for descending
+	printRDD(swappedPatents);
 
-		// Swap each tuple so we can sort by number of citations
-		JavaPairRDD<Integer, Integer> swappedPatents = patents.mapToPair(s -> s.swap());
-		printRDD(swappedPatents);
+	// Take the top N and print them
+	System.out.println();
+	swappedPatents.take(num).forEach(s -> System.out.println(s));
+	System.out.println();
 
-		// Sort in descending order by number of citations
-		swappedPatents = swappedPatents.sortByKey(false); // false for descending
-		printRDD(swappedPatents);
-
-		// Take the top N and print them
-		List<Tuple2<Integer, Integer>> topN = swappedPatents.take(num);
-		System.out.println();
-		for (Tuple2<?, ?> tuple : topN) {
-			System.out.println("(" + tuple._2() + "," + tuple._1() + ")");
-		}
-		System.out.println();
-
-		sc.stop();
-		sc.close();
-	}
+	sc.stop();
+	sc.close();
+    }
 }
