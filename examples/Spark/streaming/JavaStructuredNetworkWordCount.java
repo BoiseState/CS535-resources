f0550174fbd74dc2b4125d92fb7ed1f0f970e6e0
Amit Jain <ajain@boisestate.edu>
2021-11-30 10:44:20 -0700

    Update streaming example

--- examples/Spark/streaming/JavaStructuredNetworkWordCount.java
+++ examples/Spark/streaming/JavaStructuredNetworkWordCount.java
@@ -14,17 +14,15 @@ import org.apache.spark.sql.streaming.Trigger;
  * Counts words in UTF8 encoded, '\n' delimited text received from the network
  * every second.
  *
- * Usage: JavaStructuredNetworkWordCount <hostname> <port> <hostname> and <port>
- * describe the TCP server that Spark Streaming would connect to receive data.
+ * Usage: JavaStructuredNetworkWordCount <hostname> <port>
+ * where <hostname> and <port> * describe the TCP server that Spark Streaming 
+ * would connect to receive data.
  *
- * To run this on your local machine, you need to first run a Netcat server `$
- * nc -lk 9999` and then run the example `$ bin/run-example
- * org.apache.spark.examples.streaming.JavaNetworkWordCount localhost 9999`
+ * To run this on your local machine, check the accompanying README.md file.
  */
 public final class JavaStructuredNetworkWordCount
 {
     private static final Pattern SPACE = Pattern.compile(" ");
-
 
     public static void main(String[] args) throws Exception {
 	if (args.length < 2) {
@@ -40,7 +38,7 @@ public final class JavaStructuredNetworkWordCount
 		.format("socket")
 		.option("host", "localhost")
 		.option("port", 9999)
-	        .load();
+        .load();
 	
 	// Split the lines into words
 	Dataset<String> words = lines.as(Encoders.STRING()).flatMap(
@@ -50,8 +48,8 @@ public final class JavaStructuredNetworkWordCount
 	Dataset<Row> wordCounts = words.groupBy("value").count();
 
 	// Start running the query that prints the running counts to the console
-//	StreamingQuery query = wordCounts.writeStream().outputMode("complete")
-//	                       .format("console").start();
+	// StreamingQuery query = wordCounts.writeStream().outputMode("complete")
+	//	                       .format("console").start();
 	StreamingQuery query = wordCounts.writeStream().outputMode("complete")
 		               .format("console")
 		               .trigger(Trigger.ProcessingTime(5000))
--- examples/Spark/streaming/README.md
+++ examples/Spark/streaming/README.md
@@ -3,35 +3,24 @@ Build
 =====
 
 Build the jar file directly in Eclipse (just include all Spark jar files from
-~/spark-install/spark/jars as External jars for the project.
+~/spark-install/spark/jars as External jars for the project).
 
 Run
 ===
 
-spark-submit --class "WordCount" --master local[4] wordcount.jar input
+Startup a streaming server with the following command (in a separate terminal):
+
+nc -l 9999
+
+Then startup the streaming listener in a separate terminal as follows:
+
+spark-submit --master local[4] spark-streaming-wordcount.jar localhost 9999 2> log
 
 where local[4] says to use 4 threads on local machine. You can change that to higher or lower
 or replace by * to use as many threads as the number of logical threads on your local system.
 
-For input files on HDFS, make sure Hadoop is up and running. Then use the following 
 
-spark-submit --class "WordCount" --master local[4] wordcount.jar hdfs://localhost:9000/user/amit/input
+Now type some words in the terminal running nc. Make sure to hit enter after each line. Every 5
+seconds, the listener will trigger and collect the data and run a streaming query.
 
-
-Logging
-=======
-
-By default, spark generates a lot of info messages. You can redirect them to a file as follows:
-
-spark-submit --class "WordCount" --master local[4]  wordcount.jar input  2> log
-
-Or you can control the logging level from your program. Here is the relevant code:
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-. . .
-
-Logger log = LogManager.getRootLogger();
-log.setLevel(Level.WARN);
 
