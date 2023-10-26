5d20837a2147d7d6fbadfb609af7a013a18cd764
Amit Jain <ajain@boisestate.edu>
2023-10-26 11:44:21 -0600

    Update notes

--- examples/spark/simpleapp-python/SimpleApp.py
+++ examples/spark/simpleapp-python/SimpleApp.py
@@ -2,9 +2,9 @@
 """SimpleApp.py"""
 from pyspark.sql import SparkSession
 
-#Use same file as for the Java program to verify results
-logFile = "../simpleapp/README.md"  # Should be some file on your system
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+
+logFile = "../simpleapp/README.md"  # Should be some file on your system
 logData = spark.read.text(logFile).cache()
 
 numAs = logData.filter(logData.value.contains('a')).count()
